b776fc23b863a0cc8549db35627a38f21f664bdd
Samson Eduotu <ogheneinfinitea@gmail.com>
2018-12-06 16:53:16 -0500

    student module completed watiting for unit testing

--- app/src/main/java/com/example/root/firestoreimplemetation/getstudent.java
+++ app/src/main/java/com/example/root/firestoreimplemetation/getstudent.java
@@ -1,6 +1,83 @@
 package com.example.root.firestoreimplemetation;
 
+import com.google.firebase.firestore.Exclude;
+
 public class getstudent {
+
+    private  String Document_id;
+    private  String regno;
+    private String name;
+    private String Level;
+    private String phone;
+    private String address;
+    private String email;
+
+    public getstudent(String regno, String name, String level, String phone, String address, String email) {
+        this.regno = regno;
+        this.name = name;
+        Level = level;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+    }
+
+    @Exclude
+    public String getDocument_id() {
+        return Document_id;
+    }
+
+    public void setDocument_id(String document_id) {
+        Document_id = document_id;
+    }
+
+    public String getRegno() {
+        return regno;
+    }
+
+    public void setRegno(String regno) {
+        this.regno = regno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String level) {
+        Level = level;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 
 
 }
--- app/src/main/java/com/example/root/firestoreimplemetation/student.java
+++ app/src/main/java/com/example/root/firestoreimplemetation/student.java
@@ -2,12 +2,73 @@ package com.example.root.firestoreimplemetation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class student extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_DESCRIPTION = "description";
+
+    private EditText edttextsReg_no;
+    private EditText edttextsName;
+    private EditText edttextsPhone;
+    private EditText edttextsAddress;
+    private EditText  edttextsEmail;
+    private EditText edttextsLevel;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference notebookRef = db.collection("Student");
+    private DocumentReference noteRef = db.document("Notebook/My First Note");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+
+        edttextsReg_no = (EditText)findViewById(R.id.edttextsReg_no);
+        edttextsName = (EditText) findViewById(R.id.edttextsName);
+        edttextsPhone = (EditText) findViewById(R.id.edttextsPhone);
+        edttextsAddress = (EditText) findViewById(R.id.edttextsAddress);
+        edttextsEmail = (EditText) findViewById(R.id.edttextsEmail);
+        edttextsLevel = (EditText) findViewById(R.id.edttextsLevel);
+
     }
-}
+    public void staff_btnSave(View v){
+
+        String regno = edttextsReg_no.getText().toString();
+        String address = edttextsAddress.getText().toString();
+        String name= edttextsName.getText().toString();
+        String email = edttextsEmail.getText().toString();
+        String phone = edttextsPhone.getText().toString();
+        String level = edttextsLevel.getText().toString();
+
+        getstudent getstudent = new getstudent(regno, address,name,email,phone,level);
+        notebookRef.add(getstudent);
+
+
+
+
+
+    }
+
+
+    public void staff_btnSearch(View v){
+
+    }
+    public void staff_btnDelete(View v){
+
+    }
+    public void staff_btnUpdate(View v){
+
+    }
+
+
+    }
+
+
